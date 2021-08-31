04bfb1511a36f91a91e7e865ebaa0f1bae8ff31c
Ribz <andreas.ribbfors@icloud.com>
2021-08-31 08:45:17 +0200

    security update

--- src/Other/Sanity.js
+++ src/Other/Sanity.js
@@ -1,6 +1,6 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: "hygwusct",
-  dataset: "production",
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  dataset: process.env.REACT_APP_DATASET,
 });
